fc73e19640892873f3784950deda56d9e1d302e3
Kosa Matyas <39438549+kosa12@users.noreply.github.com>
2026-01-17 15:23:11 +0200

    1.14.1: feedback dialog and other fixes (#13)
    * 1.14.1: feedback dialog and other fixes

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
         {
           default = pkgs.rustPlatform.buildRustPackage {
             pname = "hyprkcs";
-            version = "1.14.0";
+            version = "1.14.1";
 
             src = ./.;
 
