5e35eb056067af9d7f70dd104fe27b414ba1dcba
Kosa Matyas <kosa03matyas@gmail.com>
2026-01-23 16:25:21 +0200

    1.19.1: 1.19.1: feat: system check and binary name fix (#37)

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
         {
           default = pkgs.rustPlatform.buildRustPackage {
             pname = "hyprkcs";
-            version = "1.19.0";
+            version = "1.19.1";
 
             src = let
               fs = pkgs.lib.fileset;
