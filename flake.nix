c4196242a83be056b579baaf38916ea7df798a7a
Kosa Matyas <kosa03matyas@gmail.com>
2026-01-25 20:10:25 +0200

    bump

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
         {
           default = pkgs.rustPlatform.buildRustPackage {
             pname = "hyprkcs";
-            version = "1.20.4";
+            version = "1.20.5";
 
             src = let
               fs = pkgs.lib.fileset;
