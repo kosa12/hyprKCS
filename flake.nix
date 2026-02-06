01dfb69bb62129e2326443df8d317553800218f0
Kosa Matyas <kosa03matyas@gmail.com>
2026-02-06 19:38:49 +0200

    1.24.0: feat: Omarchy colors.toml theme integration (#62)

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
         {
           default = pkgs.rustPlatform.buildRustPackage {
             pname = "hyprkcs";
-            version = "1.23.8";
+            version = "1.24.0";
 
             src = let
               fs = pkgs.lib.fileset;
