46b0412154dadb8de53a0ca798df5b0cb3cfd338
Kosa Matyas <kosa03matyas@gmail.com>
2026-02-24 19:56:07 +0200

    1.25.2: fix: Omarchy colors.toml path

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
         {
           default = pkgs.rustPlatform.buildRustPackage {
             pname = "hyprkcs";
-            version = "1.25.1";
+            version = "1.25.2";
 
             src = let
               fs = pkgs.lib.fileset;
